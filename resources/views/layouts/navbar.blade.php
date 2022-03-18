4bcbb9faacb99a31cb7989721964e449f1abe51b
nicoco1173 <rikurunico@gmail.com>
2022-03-18 22:08:42 +0700

    minor

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,8 +5,8 @@
 
     <nav id="navbar" class="navbar">
         <ul>
-        <li><a class="nav-link scrollto active" href="{{url('')}}">Home</a></li>
-        <li><a class="nav-link scrollto" href="#about">About</a></li>
+        <li><a class="nav-link scrollto active" href="{{url('home')}}">Home</a></li>
+        <li><a class="nav-link scrollto" href="{{url('about')}}">About</a></li>
         <li><a class="nav-link scrollto" href="#services">Services</a></li>
         <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
         <li><a class="nav-link scrollto" href="#team">Team</a></li>
