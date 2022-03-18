c503ef11f18bdb9225e173ce747b900f6db808f0
nicoco1173 <rikurunico@gmail.com>
2022-03-18 20:57:09 +0700

    layouting pt 2

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,22 +6,22 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="/template//template/assets/img/favicon.png" rel="icon">
-    <link href="/template//template/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="{{asset('template/assets/img/favicon.png')}}" rel="icon">
+    <link href="{{asset('template/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="/template/assets/vendor/aos/aos.css" rel="stylesheet">
-    <link href="/template/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/template/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="/template/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="/template/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="/template/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="{{asset('template/assets/vendor/aos/aos.css')}}" rel="stylesheet">
+    <link href="{{asset('template/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('template/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('template/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{asset('template/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+    <link href="{{asset('template/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="/template/assets/css/style.css" rel="stylesheet">
+    <link href="{{asset('template/assets/css/style.css')}}" rel="stylesheet">
 
     <!-- =======================================================
     * Template Name: BizLand - v3.7.0
@@ -29,3 +29,6 @@
     * Author: BootstrapMade.com
     * License: https://bootstrapmade.com/license/
     ======================================================== -->
+
+        <!-- Vendor JS Files -->
+        
