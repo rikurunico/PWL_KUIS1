11d8c64f07cfc5f90e446eed0bbe4d94bd01cb7a
nicoco1173 <rikurunico@gmail.com>
2022-03-19 10:45:36 +0700

    alert

--- resources/views/contents/home.blade.php
+++ resources/views/contents/home.blade.php
@@ -6,8 +6,8 @@
       <h1>Welcome to <span>BizLand</span></h1>
       <h2>We are team of talented designers making websites with Bootstrap</h2>
       <div class="d-flex">
-        <a href="#about" class="btn-get-started scrollto">Get Started</a>
-        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+        <a href="{{ url('about')}}" class="btn-get-started scrollto">Get Started</a>
+        <a href="#" class="glightbox btn-watch-video" onClick='alert("Belum Ada Videonya Bang")'><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
       </div>
     </div>
   </section><!-- End Hero -->
