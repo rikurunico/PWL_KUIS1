2f9b13d8b71eee3a6fbef6853b471f19c83d4c95
redo128 <aditya.alamat007@gmail.com>
2022-03-21 00:13:26 +0700

    Modif

--- resources/views/contents/testimonial.blade.php
+++ resources/views/contents/testimonial.blade.php
@@ -93,36 +93,36 @@
             <div class="col-lg-12 d-flex justify-content-center">
                 <ul id="portfolio-flters">
                 <li data-filter="*" class="filter-active">All</li>
-                <li data-filter=".filter-app">App</li>
-                <li data-filter=".filter-card">Card</li>
-                <li data-filter=".filter-web">Web</li>
+                <li data-filter=".filter-accessoris">Accessoris</li>
+                <li data-filter=".filter-card">AI</li>
+                <li data-filter=".filter-component1">Component</li>
                 </ul>
             </div>
             </div>
 
             <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                <img src="{{asset('template/assets/img/portfolio/portfolio-1.jpg')}}" class="img-fluid" alt="">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-accessoris">
+                <img src="{{asset('template/assets/img/stuff/stuff_1.jpg')}}" class="img-fluid" alt="">
                 <div class="portfolio-info">
-                <h4>App 1</h4>
-                <p>App</p>
-                <a href="{{asset('template/assets/img/portfolio/portfolio-1.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
-                <a href="{{url('more-details')}}" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                <h4>Keyboard</h4>
+                <p>Lihat Selengkapnya</p>
+                <!-- <a href="{{asset('template/assets/img/portfolio/portfolio-1.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a> -->
+                <a href="{{url('stuff')}}" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
             </div>
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                <img src="{{asset('template/assets/img/portfolio/portfolio-2.jpg')}}" class="img-fluid" alt="">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-component1">
+                <img src="{{asset('template/assets/img/BarangCover/pcc.png')}}" class="img-fluid" alt="">
                 <div class="portfolio-info">
-                <h4>Web 3</h4>
-                <p>Web</p>
-                <a href="{{asset('template/assets/img/portfolio/portfolio-2.jpg')}}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
-                <a href="{{url('more-details')}}" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                <h4>Processor</h4>
+                <p>Lihat Selengkapnya</p>
+                <a href="{{asset('template/assets/img/BarangCover/pcc.png')}}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
+                <a href="{{url('stuff')}}" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
             </div>
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-accessoris">
                 <img src="{{asset('template/assets/img/portfolio/portfolio-3.jpg')}}" class="img-fluid" alt="">
                 <div class="portfolio-info">
                 <h4>App 2</h4>
@@ -142,7 +142,7 @@
                 </div>
             </div>
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-component1">
                 <img src="{{asset('template/assets/img/portfolio/portfolio-5.jpg')}}" class="img-fluid" alt="">
                 <div class="portfolio-info">
                 <h4>Web 2</h4>
@@ -152,7 +152,7 @@
                 </div>
             </div>
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-accessoris">
                 <img src="{{asset('template/assets/img/portfolio/portfolio-6.jpg')}}" class="img-fluid" alt="">
                 <div class="portfolio-info">
                 <h4>App 3</h4>
@@ -182,7 +182,7 @@
                 </div>
             </div>
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-component1">
                 <img src="{{asset('template/assets/img/portfolio/portfolio-9.jpg')}}" class="img-fluid" alt="">
                 <div class="portfolio-info">
                 <h4>Web 3</h4>
